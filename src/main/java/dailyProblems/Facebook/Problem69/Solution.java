bc9b78302afc08299286117e5321b6ec6b9e78b4
marinelaneagu <marinela.neagu@brivo.com>
2019-08-31 00:35:54 +0300

    commit

---
+++ src/main/java/dailyProblems/Facebook/Problem69/Solution.java
@@ -0,0 +1,16 @@
+package dailyProblems.Facebook.Problem69;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public static void main(String... args) {
+        System.out.println(maxTriplet(new int[] { -10, -10, 5, 2 }));   // 500
+    }
+
+    public static int maxTriplet(int[] arr) {
+        Arrays.sort(arr);
+        int res = arr[arr.length - 1];
+        return Math.max(res, arr[arr.length - 3] * arr[arr.length - 2] * arr[arr.length - 1]);
+    }
+}
---
+++ src/main/java/problems/twoSum/Solution.java
@@ -0,0 +1,38 @@
+package problems.twoSum;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given an array of integers, return <b>indices</b> of the two numbers such that they add up to a specific target.
+ * <p>
+ * You may assume that each input would have <b>exactly</b> one solution, and you may not use the <i>same</i> element twice.
+ * <p>
+ * <b>Example:</b>
+ * <pre>
+ * Given nums = [2, 7, 11, 15], target = 9,
+ *
+ * Because nums[<b>0</b>] + nums[<b>1</b>] = 2 + 7 = 9,
+ * return [<b>0</b>, <b>1</b>].
+ * </pre>
+ *
+ */
+public class Solution {
+    public static void main(String... args) {
+        System.out.println(Arrays.toString(twoSum(new int[] { 2, 7, 11, 15 }, 9)));
+        System.out.println(Arrays.toString(twoSum(new int[] { 11, 15, 2, 7 }, 9)));
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i]))
+                return new int[] { map.get(target - nums[i]), i };
+            map.put(nums[i], i);
+        }
+
+        return null;
+    }
+}
